7f2a45d44120142e7914458c01f3cb35569b7fbb
bala-amypo <bala@amypo.in>
2025-12-17 08:48:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,7 +4,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Column;
 @Entity
 public class User{
     public enum Role{
