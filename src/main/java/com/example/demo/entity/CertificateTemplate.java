ea77d1016a277f0ed3f039666425363cf6e5ee95
bala-amypo <bala@amypo.in>
2025-12-17 10:19:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -12,5 +12,46 @@ public class CertificateTemplate{
     private Long id;
     @Column(unique=true)
     private String templateName;
-    private String backgroundUrl
+    private String backgroundUrl;
+    private String fontStyle;
+    private String signatureName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+    public String getBackgroundUrl() {
+        return backgroundUrl;
+    }
+    public void setBackgroundUrl(String backgroundUrl) {
+        this.backgroundUrl = backgroundUrl;
+    }
+
+
+    public String getFontStyle() {
+        return fontStyle;
+    }
+    public void setFontStyle(String fontStyle) {
+        this.fontStyle = fontStyle;
+    }
+    public String getSignatureName() {
+        return signatureName;
+    }
+
+    public void setSignatureName(String signatureName) {
+        this.signatureName = signatureName;
+    }
+    public CertificateTemplate(Long id,String templateName,String backgroundUrl,String fontStyle,String signatureName){
+        this.
+    }
 }
