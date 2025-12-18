509b1fe6826e4cb636b16029d43eaa0010755308
bala-amypo <bala@amypo.in>
2025-12-18 04:45:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -11,5 +11,48 @@ public class VerificationLog{
     @Id
     private Long id;
     private LocalDateTime verifiedAt;
-    public String Status 
+    public String Status={
+        SUCCESS,
+        FAILED
+    } 
+    private Status status;
+    private String ipAddress;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getVerifiedAt() {
+        return verifiedAt;
+    }
+
+    public void setVerifiedAt(LocalDateTime verifiedAt) {
+        this.verifiedAt = verifiedAt;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+  
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+    public VerificationLog(Long id,LocalDateTime verifiedAt,Status status,String ipAddress){
+        this.id=id;
+        this verifiedAt=verifiedAt;
+        this.status=status;
+        this.ipAddress=
+    }
 }
