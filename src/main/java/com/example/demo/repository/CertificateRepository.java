716bb45cf7ccc516f65eeec35fd40ec8b40fa66b
bala-amypo <bala@amypo.in>
2025-12-18 10:21:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/CertificateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateRepository.java
@@ -1,7 +1,1 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Certificate;
-public interface CertificateRepository extends JpaRepository<Certificate,Long>{
-    
-}
+// 
--- src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
@@ -1,7 +1,7 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.CertificateTemplate;
-public interface CertificateTemplateRepository extends JpaRepository<CertificateTemplate,Long>{
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.entity.CertificateTemplate;
+// public interface CertificateTemplateRepository extends JpaRepository<CertificateTemplate,Long>{
     
-}
+// }
