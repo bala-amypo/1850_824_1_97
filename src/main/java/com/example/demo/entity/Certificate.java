99a0cae5f455fb01ab6e12fe73f3ca79fdf156ab
bala-amypo <bala@amypo.in>
2025-12-18 03:40:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -5,5 +5,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import.java.time.LocalDate;
 
 @Entity
+public class Certificate{
+    @Id
+    private Long id;
+    private LocalDate issuedDate;
+    private String qrCodeUrl;
+    @Column(unique=true)
+    private String verificationCode;
+
+}
