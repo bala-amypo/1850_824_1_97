6278f138db24948d7f35c04648a2691937ccd000
bala-amypo <bala@amypo.in>
2025-12-18 08:31:39 +0000

    User commit

--- src/main/java/com/example/demo/service/VerificationService.java
+++ src/main/java/com/example/demo/service/VerificationService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import java.util.List;
 import com.example.demo.entity.VerificationLog;
 
 public interface VerificationService {
 
-    VerificationLog verifyCertificate(String verificationCode, String ipAddress);
+    VerificationLog verifyCertificate(String verificationCode, String clientIp);
+
+    List<VerificationLog> getLogsByCertificate(Long certificateId);
 }
