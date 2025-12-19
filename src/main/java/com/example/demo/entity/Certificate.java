a5ccfb58df37a5f4aa57a13d899709995bc7feb6
bala-amypo <bala@amypo.in>
2025-12-19 06:18:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -23,6 +23,63 @@ public class Certificate {
 
     @Column(unique = true)
     private String verificationCode;
+    
+   
+
+    public Long getId() {
+        return id;
+    }
+
+   
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+   
+    public Student getStudent() {
+        return student;
+    }
+
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public CertificateTemplate getTemplate() {
+        return template;
+    }
+
+   
+    public void setTemplate(CertificateTemplate template) {
+        this.template = template;
+    }
 
     
+    public LocalDate getIssuedDate() {
+        return issuedDate;
+    }
+
+    
+    public void setIssuedDate(LocalDate issuedDate) {
+        this.issuedDate = issuedDate;
+    }
+
+    
+    public String getQrCodeUrl() {
+        return qrCodeUrl;
+    }
+
+   
+    public void setQrCodeUrl(String qrCodeUrl) {
+        this.qrCodeUrl = qrCodeUrl;
+    }
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+    public Certificate(
+
+
 }
