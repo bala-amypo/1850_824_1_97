fd79e279f128ee06c55aaa4c4b6d7ef4da3b3579
bala-amypo <bala@amypo.in>
2025-12-19 08:10:52 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -1,6 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -12,79 +12,43 @@ public class VerificationLog {
 
     private LocalDateTime verifiedAt;
 
-    private String status; // SUCCESS / FAILED
+    @Enumerated(EnumType.STRING)
+    private VerificationStatus status;
 
     private String ipAddress;
 
     @ManyToOne
     private Certificate certificate;
 
-   
-
-    /**
-     * @return Long return the id
-     */
-    public Long getId() {
-        return id;
+    // ✅ REQUIRED getters & setters
+    public VerificationStatus getStatus() {
+        return status;
     }
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(Long id) {
-        this.id = id;
+    public void setStatus(VerificationStatus status) {
+        this.status = status;
     }
 
-    /**
-     * @return LocalDateTime return the verifiedAt
-     */
-    public LocalDateTime getVerifiedAt() {
-        return verifiedAt;
-    }
-
-    /**
-     * @param verifiedAt the verifiedAt to set
-     */
     public void setVerifiedAt(LocalDateTime verifiedAt) {
         this.verifiedAt = verifiedAt;
     }
 
-    /**
-     * @return String return the ipAddress
-     */
-    public String getIpAddress() {
-        return ipAddress;
-    }
-
-    /**
-     * @param ipAddress the ipAddress to set
-     */
     public void setIpAddress(String ipAddress) {
         this.ipAddress = ipAddress;
     }
 
-    /**
-     * @return Certificate return the certificate
-     */
-    public Certificate getCertificate() {
-        return certificate;
-    }
-
-    /**
-     * @param certificate the certificate to set
-     */
     public void setCertificate(Certificate certificate) {
         this.certificate = certificate;
     }
-    
-    public VerificationLog(Certificate certificate, Long id, String ipAddress, String status, LocalDateTime verifiedAt) {
-        this.certificate = certificate;
+
+    public VerificationLog(Long id, LocalDateTime verifiedAt, VerificationStatus status, String ipAddress,
+            Certificate certificate) {
         this.id = id;
-        this.ipAddress = ipAddress;
-        this.status = status;
         this.verifiedAt = verifiedAt;
+        this.status = status;
+        this.ipAddress = ipAddress;
+        this.certificate = certificate;
     }
     public VerificationLog(){}
-
 
 }
