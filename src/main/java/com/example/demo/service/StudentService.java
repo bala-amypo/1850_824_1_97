ff223ee097afcd92460c6a06ced1f82df5d78d0b
bala-amypo <bala@amypo.in>
2025-12-19 04:48:35 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,16 +1,10 @@
 package com.example.demo.service;
 
 import java.util.List;
-
 import com.example.demo.entity.Student;
 
 public interface StudentService {
-
-    // Throw exception if student email already exists
     Student addStudent(Student student);
-
     List<Student> getAllStudents();
-
-    // Throw "Student not found" if id is missing
     Student findById(Long id);
 }
